6b41e5e000ecf8ea935a2097640772d737f4dbc1
joelbkoshy <joelbkoshy@gmail.com>
2024-02-02 15:51:36 +0530

    Frontend routing

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,52 @@
-export default function LoginPage(){
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+
+
+type loginDetails = {
+    email: string,
+    password: string
+}
+
+export default function LoginPage() {
+
+
+    const [loginUser, setLoginUser] = useState<loginDetails>({
+        email: "",
+        password: ""
+    })
+
+    const onLogin = async () => {
+
+    }
+
     return (
-        <div>Hello World!</div>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Login</h1>
+            <hr />
+            <label htmlFor="email">Email</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4m focus:outline-none focus:border-gray-600"
+                id="email"
+                type="email"
+                placeholder="Email"
+                onChange={(e) => setLoginUser({ ...loginUser, email: e?.target?.value })}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+                type="password"
+                className="p-2 border border-gray-300 rounded-lg mb-4m focus:outline-none focus:border-gray-600"
+                id="password"
+                placeholder="Password"
+                onChange={(e) => setLoginUser({ ...loginUser, password: e?.target?.value })}
+            />
+
+            <button
+                onClick={onLogin}
+                className="bg-black hover:bg-gray-500 text-white font-bold py-2 px-4 border border-gray-800r mt-3 rounded">
+                Login
+            </button>
+            <Link href='/signup'>Visit Signup Page</Link>
+        </div>
     )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,65 @@
+"use client";
+import Link from "next/link"
+import { useState } from "react"
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+type userDetails = {
+    email: string,
+    password: string,
+    username: string
+}
+
 export default function SignupPage() {
+
+    const [user, setUser] = useState<userDetails>({
+        email: "",
+        password: "",
+        username: ""
+    })
+
+    const onSignup = async () => {
+
+    }
+
+
     return (
-        <div>SignUp</div>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Signup</h1>
+            <hr />
+            <label htmlFor="username">username</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4m focus:outline-none focus:border-gray-600"
+                id="username"
+                type="text"
+                value={user?.username}
+                onChange={(e) => setUser({ ...user, username: e?.target?.value })}
+                placeholder="username"
+            />
+            <hr />
+            <label htmlFor="email">email</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4m focus:outline-none focus:border-gray-600"
+                id="email"
+                type="email"
+                value={user?.email}
+                onChange={(e) => setUser({ ...user, email: e?.target?.value })}
+                placeholder="email"
+            />
+            <hr />
+            <label htmlFor="password">password</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4m focus:outline-none focus:border-gray-600"
+                id="password"
+                type="password"
+                value={user?.password}
+                onChange={(e) => setUser({ ...user, password: e?.target?.value })}
+                placeholder="password"
+            />
+            <button className="bg-black hover:bg-gray-500 text-white font-bold py-2 px-4 border border-gray-800r mt-3 rounded">
+                Button
+            </button>
+            <Link href="/login"> Visit Login Page </Link>
+        </div>
     )
 }
